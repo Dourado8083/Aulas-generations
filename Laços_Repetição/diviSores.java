5e5ef49a1c67224e93a1dce5b9469c618a91320d
Dourado8083 <72830794+Dourado8083@users.noreply.github.com>
2021-06-04 15:52:21 -0300

    Update diviSores.java

--- Laços_Repetição/diviSores.java
+++ Laços_Repetição/diviSores.java
@@ -3,7 +3,7 @@ package generation;
 public class diviSores {
     public static void main(String[] args) {
         //exercício 1 automatizado
-        for (int x=1000; x < 1999; x++){
+        for (int x=1000; x <= 1999; x++){
                 if (x%11==5) {
                     System.out.println(x);
                 }
