653d7753daeb6ef55ace6ea809aadd4bf61d6bca
Dourado8083 <gustavo.dourado.br@gmail.com>
2021-06-06 15:58:48 -0300

    Create IdadePerdidacomCigarro .java

---
+++ Exercícios por fora/IdadePerdidacomCigarro .java
@@ -0,0 +1,15 @@
+package generation;
+import java.util.Scanner;
+public class IdadePerdidacomCigarro  {
+    public static void main(String[] args) {
+        int ano,qtd,diaspedi=0,total=0;
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite o quantidade de cigarro que você fuma:");
+        qtd = ler.nextInt();
+        System.out.println("Digite quantos anos você fuma:");
+        ano =  ler.nextInt();
+        total =(ano * 365) * qtd; //anos 
+        diaspedi = (total * 10) /24; //10 minutos duração de um cigarro
+        System.out.println("Você perdeu : "+diaspedi +" anos ");
+        }   
+}
